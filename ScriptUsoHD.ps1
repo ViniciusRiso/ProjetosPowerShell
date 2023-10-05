6e63cfaf313772ac80842678b427c21c9c3351fc
Vinicius Riso <Vinicius.ag.riso@gmail.com>
2023-10-05 18:58:46 -0300

    Enviando meus arquivos

---
+++ ScriptUsoHD.ps1
@@ -0,0 +1,11 @@
+﻿# Parameters
+param([String]$dir = "C:\Users\vinic\Downloads")
+
+# Function
+function Get-Dir-Info($dir) {
+    $results = Get-ChildItem $dir -Recurse | Measure-Object -Property Length -Sum
+    return [math]::Round(($results.Sum / 1GB), 2)
+}
+
+Get-dir-info $dir 
+get-dir-info C:\Users\vinic\Documents
